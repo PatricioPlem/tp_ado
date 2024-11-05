a17d3b643ca8aec36b949869a33ec5b0bbd46611
Patricio Plem <patorafael.2000@gmail.com>
2024-11-04 21:06:40 -0300

    que tipo pelotudo que soy

---
+++ gympal/src/test/java/com/uade/gympal/GympalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uade.gympal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GympalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
