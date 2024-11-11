c9856ea35f86b3f2f4c294d04ff7d3192c56a715
Patricio Plem <patorafael.2000@gmail.com>
2024-11-11 02:09:33 -0300

    endpoint crear ejercicios nuevos

--- gympal/src/main/java/com/uade/gympal/Controller/EjercicioController.java
+++ gympal/src/main/java/com/uade/gympal/Controller/EjercicioController.java
@@ -2,6 +2,7 @@ package com.uade.gympal.Controller;
 
 import com.uade.gympal.DTO.EjercicioCompletadoDTO;
 import com.uade.gympal.Repository.Entity.EjercicioImplementado;
+import com.uade.gympal.Repository.Entity.EjercicioTemplate;
 import com.uade.gympal.Service.EjercicioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -26,4 +27,17 @@ public class EjercicioController {
         }
     }
 
+    @PostMapping("/crear")
+    public ResponseEntity<EjercicioTemplate> crearEjercicioTemplate(@RequestBody EjercicioTemplate ejercicio) {
+        try {
+
+            // Guardar el ejercicio utilizando el servicio correspondiente (puede ser un servicio de Ejercicio)
+            EjercicioTemplate creado = ejercicioService.crearEjercicio(ejercicio);
+
+            // Retornar la respuesta con el ejercicio creado
+            return ResponseEntity.status(HttpStatus.CREATED).body(creado);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error al crear el ejercicio: " + e.getMessage());
+        }
+    }
 }
--- gympal/src/main/java/com/uade/gympal/Service/EjercicioService.java
+++ gympal/src/main/java/com/uade/gympal/Service/EjercicioService.java
@@ -3,8 +3,10 @@ package com.uade.gympal.Service;
 import com.uade.gympal.DTO.EjercicioCompletadoDTO;
 import com.uade.gympal.Repository.EjercicioImplementadoRepository;
 
+import com.uade.gympal.Repository.EjercicioRepository;
 import com.uade.gympal.Repository.Entity.EjercicioImplementado;
 
+import com.uade.gympal.Repository.Entity.EjercicioTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,8 @@ public class EjercicioService {
 
     @Autowired
     private EjercicioImplementadoRepository ejercicioImplementadoRepository;
+    @Autowired
+    private EjercicioRepository ejercicioRepository;
 
     public EjercicioImplementado completarEjercicio(EjercicioCompletadoDTO ejercicio) {
         EjercicioImplementado ejercicioImplementado = ejercicioImplementadoRepository.findById(ejercicio.getId()).orElseThrow();
@@ -25,6 +29,10 @@ public class EjercicioService {
         ejercicioImplementado.setSeriesRealizadas(ejercicio.getSeries());
 
         return ejercicioImplementadoRepository.save(ejercicioImplementado);
+    }
+
+    public EjercicioTemplate crearEjercicio(EjercicioTemplate ejercicio) {
+        return ejercicioRepository.save(ejercicio);
     }
 
 }
